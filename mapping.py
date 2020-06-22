98a243c4d645b8155534fbf875339f4a9183e479
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-22 16:16:30 +0200

    add  volcanoes file, import it in the .py and extract the longitudes and latitudes

--- mapping.py
+++ mapping.py
@@ -2,20 +2,32 @@
 
 
 import folium
+import pandas as pd
+
+#### Import data from a csv file
+data = pd.read_csv("vulcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
  #### Create a base map object with folium centered to the latitude and logitide
  #### with initial zoom of 6 and a layer on top (Stamen Terrain layer)
-map = folium.Map(location=[59.263794608921565, 18.021744513185972],
+map = folium.Map(location=[45, -100],
                 zoom_start=6, tiles = "Stamen Terrain")
 
 #### Add a child to the map with markers
 fg = folium.FeatureGroup(name="My Map") # features creation
 
 ### multiple markers
-for coordinates in [[59.1, 18.01], [58.8, 17.5]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Home",
-                icon=folium.Icon(color='green')))
-map.add_child(fg)
+# for coordinates in [[59.1, 18.01], [58.8, 17.5]]:
+#     fg.add_child(folium.Marker(location=coordinates, popup="Home",
+#                 icon=folium.Icon(color='green')))
 
+### Extract latitude and logitude from the csv
+for lt, ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt,ln], popup="Home",
+                    icon=folium.Icon(color='green')))
+
+
+map.add_child(fg)
  #### Save the map in an htlm file to load it with the browser
 map.save("Map1.html")
