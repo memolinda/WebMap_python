1adc0974bc927016a67c377d73d18fe4d05fb81e
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-22 14:27:49 +0200

    added a layer: Stamen Terrain

--- mapping.py
+++ mapping.py
@@ -1,7 +1,12 @@
+'''Create a Webmap in Python using Folium (from the The Python Mega Course)'''
+
+
 import folium
 
- #### Create a map centered to the latitude and logitide with initial zoom of 6
-map = folium.Map(location=[59.263794608921565, 18.021744513185972], zoom_start=6)
+ #### Create a base map with folium centered to the latitude and logitide
+ #### with initial zoom of 6 and a layer on top (Stamen Terrain layer)
+map = folium.Map(location=[59.263794608921565, 18.021744513185972], zoom_start=6,
+                tiles = "Stamen Terrain")
 
  #### Save the map in an htlm file to load it with the browser
 map.save("Map1.html")
