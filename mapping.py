2ec87f6385435a5f951ca5c6b0dac6fc52ba98aa
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-22 15:07:57 +0200

    create feature, adding multiple markers

--- mapping.py
+++ mapping.py
@@ -5,11 +5,17 @@ import folium
 
  #### Create a base map object with folium centered to the latitude and logitide
  #### with initial zoom of 6 and a layer on top (Stamen Terrain layer)
-map = folium.Map(location=[59.263794608921565, 18.021744513185972], zoom_start=6,
-                tiles = "Stamen Terrain")
+map = folium.Map(location=[59.263794608921565, 18.021744513185972],
+                zoom_start=6, tiles = "Stamen Terrain")
 
 #### Add a child to the map with markers
-map.add_child(folium.Marker(location=[59.10, 18.1], popup="Hi!!", icon=folium.Icon(color='green')))
+fg = folium.FeatureGroup(name="My Map") # features creation
+
+### multiple markers
+for coordinates in [[59.1, 18.01], [58.8, 17.5]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Home",
+                icon=folium.Icon(color='green')))
+map.add_child(fg)
 
  #### Save the map in an htlm file to load it with the browser
 map.save("Map1.html")
