2c49000ec5b077c0613824b7b4efabe2c365f067
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-22 16:59:18 +0200

    color of markers depending of the elevation value

--- mapping.py
+++ mapping.py
@@ -18,6 +18,15 @@ Volcano name:<br>
 Height: %s m
 """
 
+#### Color markers depending of the elevation
+def color_icons(elev):
+    if elev < 1000:
+        return 'green'
+    elif elev>=1000 and elev<2000:
+        return 'orange'
+    else:
+        return 'red'
+
  #### Create a base map object with folium centered to the latitude and logitide
  #### with initial zoom of 6 and a layer on top (Stamen Terrain layer)
 map = folium.Map(location=[45, -100],
@@ -32,10 +41,10 @@ fg = folium.FeatureGroup(name="My Map") # features creation
 #                 icon=folium.Icon(color='green')))
 
 ### Extract latitude and logitude from the csv
-for lt, ln, el, name in zip(lat,lon,elev, name):
+for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100) #add the elevation and the link
     fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(iframe),
-                    icon=folium.Icon(color='green')))
+                    icon=folium.Icon(color=color_icons(el))))
 
 
 map.add_child(fg)
