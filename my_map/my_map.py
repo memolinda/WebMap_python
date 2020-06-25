5c17beec092a59f95cd40227aad96161173694de
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-25 16:13:58 +0200

    added marker for life and work categories

--- my_map/my_map.py
+++ my_map/my_map.py
@@ -19,13 +19,32 @@ Pictures:<br>
 <a href="https://www.google.com/search?q=%%22%s%%22&tbm=isch&ved" target="_blank">%s</a></div>
 """
 
+html_life = """<div style="Font-family: arial;">
+<strong>%s</strong> (%s): <br>
+Pictures:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22&tbm=isch&ved" target="_blank">%s</a></div>
+"""
 
 ff_home = folium.FeatureGroup(name="Home")
+ff_life = folium.FeatureGroup(name="Life")
+ff_work = folium.FeatureGroup(name="Work")
+
+
 for index, row in df_home.iterrows():
     iframe = folium.IFrame(html=html_home % (row['City'], row['Country'], row['Description'], row['City'], row['City']), width=200, height=100)
     ff_home.add_child(folium.Marker(location=[row['Lat'],row['Lon']], popup=folium.Popup(iframe), icon=folium.Icon(color='red', icon='home', prefix='fa')))
 
+for index, row in df_life.iterrows():
+    iframe = folium.IFrame(html=html_life % (row['City'], row['Country'], row['City'], row['City']), width=200, height=100)
+    ff_life.add_child(folium.Marker(location=[row['Lat'],row['Lon']], popup=folium.Popup(iframe), icon=folium.Icon(color='green', icon='gratipay', prefix='fa')))
+
+for index, row in df_work.iterrows():
+    iframe = folium.IFrame(html=row['Site'], width=200, height=100)
+    ff_work.add_child(folium.Marker(location=[row['Lat'],row['Lon']], popup=folium.Popup(iframe), icon=folium.Icon(color='blue', icon='briefcase', prefix='fa')))
+
 map.add_child(ff_home)
+map.add_child(ff_life)
+map.add_child(ff_work)
 
 
 map.add_child(folium.LayerControl())
