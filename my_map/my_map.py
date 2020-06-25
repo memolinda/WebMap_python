eec19142c3b598d89d8f60fccbd09daff2f0fb2b
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-25 15:39:44 +0200

    import jsons as Dataframes and implemented

--- my_map/my_map.py
+++ my_map/my_map.py
@@ -1,19 +1,18 @@
 import folium
 import json
+import pandas as pd
 
-home_data = json.load(open("my_data/Home.json"))
-home_city = home_data['City']
-home_country = home_data['Country']
-home_desc = home_data['Description']
-home_lat = home_data['Lat']
-home_lon = home_data['Lon']
+### Create DataFrame from json files
+df_home = pd.read_json("my_data/Home.json")
+df_life = pd.read_json("my_data/Life.json")
+df_work = pd.read_json("my_data/Work.json")
 
 ### Map centered in Berlin
 map = folium.Map(location=[52.528917, 13.331167],
                 zoom_start=4, tiles= 'Stamen Watercolor')
 
 ### Addresses for the pop up windows: City name, description, pictures
-html = """<div style="Font-family: arial;">
+html_home = """<div style="Font-family: arial;">
 <strong>%s</strong> (%s): <br>
 %s. <br>
 Pictures:<br>
@@ -22,11 +21,13 @@ Pictures:<br>
 
 
 ff_home = folium.FeatureGroup(name="Home")
-for name, country, desc, lat, lon in zip(home_city, home_country, home_desc, home_lat, home_lon):
-    iframe = folium.IFrame(html=html % (name, country, desc, name, name), width=200, height=100)
-    ff_home.add_child(folium.Marker(location=[lat,lon], popup=folium.Popup(iframe), icon=folium.Icon(color='red', icon='home', prefix='fa')))
+for index, row in df_home.iterrows():
+    iframe = folium.IFrame(html=html_home % (row['City'], row['Country'], row['Description'], row['City'], row['City']), width=200, height=100)
+    ff_home.add_child(folium.Marker(location=[row['Lat'],row['Lon']], popup=folium.Popup(iframe), icon=folium.Icon(color='red', icon='home', prefix='fa')))
 
 map.add_child(ff_home)
+
+
 map.add_child(folium.LayerControl())
 
 map.save("My_map.html")
