80ffae1747ec4c9edc92f8257bc79f8858d4587e
Linda Giacomozzi <linda.giacomozzi@gmail.com>
2020-06-26 13:52:35 +0200

    fixed problem with links for the work markers

--- my_map/my_map.py
+++ my_map/my_map.py
@@ -8,8 +8,8 @@ df_life = pd.read_json("my_data/Life.json")
 df_work = pd.read_json("my_data/Work.json")
 
 ### Map centered in Berlin
-map = folium.Map(location=[52.528917, 13.331167],
-                zoom_start=4, tiles= 'Stamen Watercolor')
+map = folium.Map(location=[34.913630, 33.213540],
+                zoom_start=2, tiles= 'Stamen Watercolor')
 
 ### Addresses for the pop up windows: City name, description, pictures
 html_home = """<div style="Font-family: arial;">
@@ -25,6 +25,12 @@ Pictures:<br>
 <a href="https://www.google.com/search?q=%%22%s%%22&tbm=isch&ved" target="_blank">%s</a></div>
 """
 
+html_work = """<div style="Font-family: arial;">
+%s : <br>
+<a href="%s" target="_blank">%s</a></div>
+"""
+'<a href="'+ 'give your url here'+'"target="_blank">'+ 'popup text' + '</a>'
+
 ff_home = folium.FeatureGroup(name="Home")
 ff_life = folium.FeatureGroup(name="Life")
 ff_work = folium.FeatureGroup(name="Work")
@@ -39,7 +45,7 @@ for index, row in df_life.iterrows():
     ff_life.add_child(folium.Marker(location=[row['Lat'],row['Lon']], popup=folium.Popup(iframe), icon=folium.Icon(color='green', icon='gratipay', prefix='fa')))
 
 for index, row in df_work.iterrows():
-    iframe = folium.IFrame(html=row['Site'], width=200, height=100)
+    iframe = folium.IFrame(html=html_work % (row['Description'], row['Site'], row['City']), width=150, height=70)
     ff_work.add_child(folium.Marker(location=[row['Lat'],row['Lon']], popup=folium.Popup(iframe), icon=folium.Icon(color='blue', icon='briefcase', prefix='fa')))
 
 map.add_child(ff_home)
